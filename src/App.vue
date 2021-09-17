ddb2b2658ef5c33aea4c48e551c239fb3a8c70da
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-17 16:39:07 +0200

    active

--- src/App.vue
+++ src/App.vue
@@ -25,30 +25,32 @@ export default {
         {
           text: "Home",
           url: "#",
+          current: true,
         },
         {
           text: "Services",
           url: "#",
+          current: false,
         },
         {
           text: "About",
           url: "#",
+          current: false,
         },
         {
           text: "Videos",
           url: "#",
+          current: false,
         },
         {
           text: "Blog",
           url: "#",
+          current: false,
         },
         {
           text: "Store",
           url: "#",
-        },
-         {
-          text: "Store",
-          url: "#",
+          current: false,
         },
       ],
     };
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,27 @@
 <template>
-  <header class="d-flex align-items-center justify-content-around">
-    <figure>
-      <img :src="logo" alt="logo" />
-    </figure>
-    <nav>
-      <ul>
-        <li v-for="(menu, index) in menus" :key="index">
-          <a class="ms-4 text-grey" 
-          :class="{active: menu.current}" 
-          :href="menu.url">{{ menu.text }}</a>
-        </li>
-        <span class="news ms-2">new</span>
-        <button class="ms-5 px-4 py-2">Schedule a workout</button>
-      </ul>
-    </nav>
+  <header>
+    <div class="container">
+      <div class="d-flex justify-content-between">
+          <figure>
+            <img :src="logo" alt="logo" />
+          </figure>
+          <nav>
+            <ul>
+              <li v-for="(menu, index) in menus" :key="index">
+                <a
+                  class="ms-4 text-grey"
+                  :class="{ active: menu.current }"
+                  :href="menu.url"
+                  >{{ menu.text }}</a
+                >
+              </li>
+              <span class="news ms-2">new</span>
+              <button class="ms-5 px-4 py-2">Schedule a workout</button>
+            </ul>
+          </nav>
+        </div>
+    </div>
+     
   </header>
 </template>
 
@@ -26,17 +34,15 @@ export default {
 
 <style lang="scss" scoped>
 @import "../assets/styles/_mixin.scss";
-   li {  
-    a:hover,
-    a.active {
-      color: #ffff;
-     
-    }
-   }
+li {
+  a:hover,
+  a.active {
+    color: #ffff;
+  }
+}
 
- button {
-        @include buttons("blue");
-        font-size: 12px;
-      }
-
+button {
+  @include buttons("blue");
+  font-size: 12px;
+}
 </style>
